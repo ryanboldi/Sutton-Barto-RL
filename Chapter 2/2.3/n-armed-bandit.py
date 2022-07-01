f5e10ebfa0002780e9fd44fde26f0165555d94b3
Ryan Boldi <ryan.boldi123@gmail.com>
2022-07-01 14:55:18 -0400

    plotted figures

--- Chapter 2/2.3/n-armed-bandit.py
+++ Chapter 2/2.3/n-armed-bandit.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
 class NArmedBandit:
     def __init__(self, n, mean, sd, sample_noise):
         self.n = n
@@ -9,6 +12,8 @@ class NArmedBandit:
     #generates an observation of the true value (noisy)
     def sample_val(self, i):
         return np.random.normal(self.true_values[i], self.sample_noise)
+
+
 class EpGreedy:
     def __init__(self, n, Ep, bandit):
         self.n = n
@@ -45,3 +50,31 @@ class EpGreedy:
         self._rewards_collected.append(sampled_val)
         #print(move_to_make)
         self.add_observation(move_to_make, sampled_val)
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+
+ep01_rewards = []
+ep001_rewards = []
+ep1_rewards = []
+for j in range(0, 2000):
+    b = NArmedBandit(10, 0, 1, 1)
+    e01 = EpGreedy(10, 0.1, b)
+    e001 = EpGreedy(10, 0.01, b)
+    e1 = EpGreedy(10, 0, b)
+    for i in range(0, 1000):
+        e01.make_move()
+        e001.make_move()
+        e1.make_move()
+    ep01_rewards.append(e01._rewards_collected)
+    ep001_rewards.append(e001._rewards_collected)
+    ep1_rewards.append(e1._rewards_collected)
+
+
+plt.title("n-armed-bandit, n=10, Q*(a)~N(0, 1), Q_t(a)~N(Q*(a), 1)")
+plt.plot(np.mean(ep01_rewards, 0), label="Epsilon = 0.1")
+plt.plot(np.mean(ep001_rewards, 0), label="Epsilon = 0.01")
+plt.plot(np.mean(ep1_rewards, 0), label="Epsilon = 0 (greedy)")
+plt.legend()
+plt.show()
+
+#print(e._rewards_collected)
