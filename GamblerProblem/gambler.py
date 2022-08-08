27047c042ab8fa32cf577a0d08b790b83959316a
Ryan Boldi <ryan.boldi123@gmail.com>
2022-08-08 09:31:44 -0400

    finished gambler problem

--- GamblerProblem/gambler.py
+++ GamblerProblem/gambler.py
@@ -1,23 +1,71 @@
-import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 goal = 100
-p = 0.4
+p = 0.55
+gamma = 1
 
-states = [0 for i in range(1, goal)]
-stateVals = [0 for s in range(0, goal+1)]
-stateVals[goal+1] = 1
+stateVals = [0 for s in range(goal+1)]
+reward = [0 for _ in range(goal+1)]
+reward[goal] = 1
+policy = [0 for _ in range(goal+1)]
 
-def valueOfState(state, action):
-    global p, goal
-
+def valueOfState(state):
     #iterate over every possible future state from this position given the possible actions
-        #get p, reward
-
-    #heads
-    if random.random() < 0.4:
-        #if heads, state is incremented by action
-        state = max(goal, state - action)
-    else:
-        state = max(0, state - action)
+    best_val = 0
     
+    for a in range(0, min(state, goal-state) + 1):
+        score = 0   
+        #states if win or loss
+        sIfWin = state + a
+        sIfLoss = state - a
 
+        score += p * (reward[sIfWin] + (gamma * stateVals[sIfWin]))
+        score += (1-p) * (reward[sIfLoss] + (gamma * stateVals[sIfLoss]))
+
+        if score > best_val:
+            best_val = score 
+
+    return best_val
+
+def get_best_policy(state):
+    #iterate over every possible future state from this position given the possible actions
+    best_val = 0
+    
+    for a in range(0, min(state, goal-state) + 1):
+        score = 0   
+        #states if win or loss
+        sIfWin = state + a
+        sIfLoss = state - a
+
+        score += p * (reward[sIfWin] + (gamma * stateVals[sIfWin]))
+        score += (1-p) * (reward[sIfLoss] + (gamma * stateVals[sIfLoss]))
+
+        if score > best_val:
+            best_val = score 
+            best_act = a
+
+    return best_act
+
+while(True): 
+    delta = 0
+    #not S+, just S
+    for s in range(1, goal):
+        v = stateVals[s]
+        actual = valueOfState(s) 
+        stateVals[s] = actual
+        delta = max(delta, abs(v - actual))
+    if delta < 0.00000001:
+        break
+
+for i in range(1, goal):
+    policy[i] = get_best_policy(i)
+
+print(policy)
+#print(stateVals)
+
+plt.scatter([i for i in range(goal+1)], policy)
+plt.xlabel("Amount of Money ($)")
+plt.ylabel("Stake (policy)")
+plt.title("Gambler's Problem, p=" + str(p))
+plt.show()
